2946ecb7037856aacf71ffcc6d10309577748457
Denis Tyurkov <nsgaming@Deniss-MacBook-Pro.local>
2014-11-04 21:33:17 +1300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 
 
@@ -32,6 +33,9 @@
     
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
+    
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
 }
 
 // called on every touch in this scene
